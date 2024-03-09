9c4122500d5287befc217bfd0b6ac0eef4f268f9
May Cook <benrcook.email@gmail.com>
2024-03-09 19:42:43 +0000

    Add fake functionality to button

--- episode-picker/components/results-card.jsx
+++ episode-picker/components/results-card.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react"
+
 export default function ResultsCard() {
+  const [episode, SetEpisode] = useState(1)
+
   return (
     <div className="results-card">
-      <p>Episode: Placeholder of death</p>
-      <p>Link: https://Placeholder.com </p>
-      <button>Pick new episode</button>
+      <p>Episode: Episode {episode} of death</p>
+      <p>Link: https://episode{episode}.com </p>
+      <button
+        onClick={() => {
+          SetEpisode(Math.floor(Math.random() * 10)) // Sets the episode to a random integer from 0-10 inclusive
+        }}
+      >
+        Pick new episode
+      </button>
     </div>
   )
 }
