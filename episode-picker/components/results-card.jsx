1e66966ea48968316122842b162f2b59c1ff26d7
May Cook <benrcook.email@gmail.com>
2024-03-09 18:31:05 +0000

    Create placeholder results card

---
+++ episode-picker/components/results-card.jsx
@@ -0,0 +1,8 @@
+export default function ResultsCard() {
+  return (
+    <div className="results-card">
+      <p>Episode: Placeholder of death</p>
+      <p>Link: https://Placeholder.com </p>
+    </div>
+  )
+}
--- episode-picker/src/App.jsx
+++ episode-picker/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css"
+import ResultsCard from "../components/results-card"
 
 function App() {
   return (
     <>
       <div>
         <h1>Doctor Who Episode Picker</h1>
+        <ResultsCard />
       </div>
     </>
   )
