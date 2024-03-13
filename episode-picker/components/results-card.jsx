533881870f711383ae6c8d2f2cb51758b9cbf66b
May Cook <benrcook.email@gmail.com>
2024-03-13 05:35:58 +0000

    Add era section to results card

--- episode-picker/components/results-card.jsx
+++ episode-picker/components/results-card.jsx
@@ -8,7 +8,7 @@ export default function ResultsCard() {
     <div className="results-card">
       <a href={episode.link} className="episodeTitle">{episode.title}</a>
       <p>Series: {episode.series}, Episode: {episode.episode}</p>
-      <p>{episode.doctor}th Doctor</p>
+      <p>{episode.doctor}th Doctor, {episode.era} era</p>
 
       <button
         onClick={() => {
